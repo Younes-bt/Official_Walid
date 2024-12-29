5f7fcf6746dab452e253e34f0af83f74d9cd6ddd
younes El bettate <bt.younesse@gmail.com>
2024-12-29 12:07:02 +0100

    add youtube videos Models

--- Official_Walid/website/models.py
+++ Official_Walid/website/models.py
@@ -32,3 +32,16 @@ class Comments(models.Model):
     content = models.TextField(null=False, blank=False)
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments")
     created_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f'{self.author} commented on post {self.article.title}'
+
+class YoutubeVideos(models.Model):
+    vidLink = models.CharField(max_length=1000, null=False, blank=False)
+    title = models.CharField(max_length=1500, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
